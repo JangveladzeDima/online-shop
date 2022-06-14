132c0e207560d5397acb009cba4b768f990a683d
JangveladzeDima <djangveladze@unisens.ge>
2022-06-14 12:57:24 +0400

    changes

--- client/src/service/client/client.service.ts
+++ client/src/service/client/client.service.ts
@@ -8,30 +8,32 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ClientService implements IClientService {
-  constructor(
-    @Inject(ClientRepository)
-    private readonly clientRepository: IClientRepository,
-    @Inject('HASH_SERVICE') private readonly hashService: ClientProxy,
-  ) {}
-
-  async getClient(filter: IClientFilter): Promise<IClient> {
-    return this.clientRepository.getClient(filter);
-  }
-
-  async addClient(clientParams: IClient): Promise<IClient> {
-    const client = await this.clientRepository.getClient({
-      email: clientParams.email,
-    });
-    if (client) {
-      throw new RpcException({
-        message: 'Email already exists',
-        code: 406,
-      });
+    constructor(
+        @Inject(ClientRepository)
+        private readonly clientRepository: IClientRepository,
+        @Inject('HASH_SERVICE') private readonly hashService: ClientProxy,
+    ) {
     }
-    const newClient = await this.clientRepository.create(clientParams);
-    return newClient;
-  }
-  deleteClient(filter: IClientFilter): Promise<IClient> {
-    return this.clientRepository.deleteClient(filter);
-  }
+
+    async getClient(filter: IClientFilter): Promise<IClient> {
+        return this.clientRepository.getClient(filter);
+    }
+
+    async addClient(clientParams: IClient): Promise<IClient> {
+        const client = await this.clientRepository.getClient({
+            email: clientParams.email,
+        });
+        if (client) {
+            throw new RpcException({
+                message: 'Email already exists',
+                code: 406,
+            });
+        }
+        const newClient = await this.clientRepository.create(clientParams);
+        return newClient;
+    }
+
+    deleteClient(filter: IClientFilter): Promise<IClient> {
+        return this.clientRepository.deleteClient(filter);
+    }
 }
